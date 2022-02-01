53fc38573247b9d9479ffc3c6679dac9ad2cdfd4
Gyrkora <giselle.gallardo0210@gmail.com>
2022-02-01 00:02:47 -0500

    empezando con tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,52 @@
+
+
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    
+    extend: {
+      maxWidth: {
+        '1/4': '25%',
+        '1/2': '300px',
+        '3/4': '80%',
+        '50' : '50rem',
+      },
 
-    colors: {
-
-      transparent: 'transparent',
-      current: 'currentColor',
-   
-      //nav
-      'hover': '#C8EFF8',
-      'iconPerfil': '#4B94C1',
-      
-      
-      //hero redes sociales
-      'facebook': '#226EBE',
-      'instagram': '#F28907',
-      'compartir': '#A9D941',
-      
-      
-      //Preguntas frecuenes
-      'fondoSeccionPreguntas': '#9CC8D9',
-      'colortitulo': '#2C3673', //se parece mucho al del footer
-
-      //formulario:
-      'verdeBoton': '#78BF45',
-      'tabs': '#4E98BD',
-      'backgroundMain': '#F2F2F2',
-
-      //footer
-      'moradoFooter': '#2D3073',
-    }
-
-  },
+      width: {
+        '80': '80%',
+      },
+      colors: {
+        'custom-yellow':'#BAA333',
+      }  
+    },
+  },  
   plugins: [],
 }
+
+
+// transparent: 'transparent',
+        // current: 'currentColor',
+     
+        // //nav
+        // 'hover': '#C8EFF8',
+        // 'iconPerfil': '#4B94C1',
+        
+        
+        // //hero redes sociales
+        // 'facebook': '#226EBE',
+        // 'instagram': '#F28907',
+        // 'compartir': '#A9D941',
+        
+        
+        // //Preguntas frecuenes
+        // 'fondoSeccionPreguntas': '#9CC8D9',
+        // 'colortitulo': '#2C3673', //se parece mucho al del footer
+  
+        // //formulario:
+        // 'verdeBoton': '#78BF45',
+        // 'tabs': '#4E98BD',
+        // 'backgroundMain': '#F2F2F2',
+  
+        // //footer
+        // 'moradoFooter': '#2D3073',
