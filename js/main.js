3c018afdc86d29d35e7e7cafe07f2dee74563f76
Gyrkora <giselle.gallardo0210@gmail.com>
2022-02-08 23:15:52 -0500

    rutas

--- js/main.js
+++ js/main.js
@@ -2,25 +2,25 @@
  const cardCategories = [
   {
     id: "gramatical",
-    url: "../src/pages/gramaticales.html",
+    url: "src/pages/gramaticales.html",
     bg: "hover:bg-verde-TOW-O"
   },
 
   {
     id: "geografico",
-    url: "../src/pages/geografico.html",
+    url: "src/pages/geografico.html",
     bg: "hover:bg-celeste-TOW"
   },
 
   {
     id: "pronunciacion",
-    url: "../src/pages/pronunciacion.html",
+    url: "src/pages/pronunciacion.html",
     bg: "hover:bg-cafe-TOW"
   },
 
   {
     id: "cultural",
-    url: "../src/pages/cultural.html",
+    url: "src/pages/cultural.html",
     bg: "hover:bg-violet-400"
   },
 ];
